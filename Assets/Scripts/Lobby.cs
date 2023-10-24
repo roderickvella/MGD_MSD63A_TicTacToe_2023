1c99ec56156b5b5439f3c8c32c7843d529a7fd8d
Roderick Vella Galea <roderick.vella@mcast.edu.mt>
2023-10-24 14:13:54 +0200

    Lesson4

--- Assets/Scripts/Lobby.cs
+++ Assets/Scripts/Lobby.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Lobby : MonoBehaviourPunCallbacks
 {
@@ -31,6 +32,11 @@ public class Lobby : MonoBehaviourPunCallbacks
     [Tooltip("Panel Waiting for Players")]
     public GameObject PanelWaitingForPlayers;
 
+    List<RoomInfo> availableRooms = new List<RoomInfo>();
+
+    UnityEngine.Events.UnityAction buttonCallback;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -56,6 +62,62 @@ public class Lobby : MonoBehaviourPunCallbacks
         print("OnConnectedToMaster");
         //after we connect to master server, we have to join a lobby
         PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+
+    public void CreateRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsOpen = true;
+        roomOptions.IsVisible = true;
+        roomOptions.MaxPlayers = 2;
+
+        PhotonNetwork.JoinOrCreateRoom(InputRoomName.GetComponent<TMP_InputField>().text, 
+            roomOptions, TypedLobby.Default);
+
+
+    }
+
+    public override void OnCreatedRoom()
+    {
+        print("Created room");
+        //set our player name (nickname)
+        PhotonNetwork.NickName = InputPlayerName.GetComponent<TMP_InputField>().text;
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        print("room creation failed:"+message);
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        print("Number of rooms:" + roomList.Count);
+        availableRooms = roomList;
+        UpdateRoomList();
+    }
+
+    private void UpdateRoomList()
+    {
+        foreach(RoomInfo roomInfo in availableRooms)
+        {
+            GameObject rowRoom = Instantiate(RowRoom);
+            rowRoom.transform.parent = ScrollViewContent.transform;
+            rowRoom.transform.localScale = Vector3.one;
+
+            rowRoom.transform.Find("RoomName").GetComponent<TextMeshProUGUI>().text = roomInfo.Name;
+            rowRoom.transform.Find("RoomPlayers").GetComponent<TextMeshProUGUI>().text = roomInfo.PlayerCount.ToString();
+
+            buttonCallback = () => this.OnClickJoinRoom(roomInfo.Name);
+            rowRoom.transform.Find("BtnJoin").GetComponent<Button>().onClick.AddListener(buttonCallback);
+
+        }
+    }
+
+    private void OnClickJoinRoom(string roomName)
+    {
+        //set our player name (nickname)
+        PhotonNetwork.NickName = InputPlayerName.GetComponent<TMP_InputField>().text;
+        //join the room
+        PhotonNetwork.JoinRoom(roomName);
     }
 
     // Update is called once per frame
